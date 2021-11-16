915fd37b214b68471e7d8717ec6c35e67adbb68e
mackah666 <mackah666@yahoo.co.uk>
2021-11-16 19:54:19 +0000

    first commit

---
+++ examples/lambda-sqs-trigger/main.tf
@@ -0,0 +1,82 @@
+resource "aws_kms_key" "this" {
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  is_enabled               = true
+  enable_key_rotation      = true
+  deletion_window_in_days  = 30
+}
+
+resource "aws_sqs_queue" "dlq" {
+  name = "tf-example-lambda-sqs-trigger-dlq"
+  kms_master_key_id = aws_kms_key.this.id
+  
+}
+
+resource "aws_sqs_queue" "lambda_example" {
+  name              = "tf-example-lambda-sqs-trigger"
+  kms_master_key_id = aws_kms_key.this.id
+}
+
+locals {
+  arn = aws_sqs_queue.lambda_example.arn
+}
+
+module "sqs_trigger" {
+  source = "../../lambda_function"
+
+  function_name         = "tf-example-lambda-sqs-trigger"
+  description           = "example lambda triggered by sqs queue"
+  runtime               = "java8"
+  handler               = "example.Hello::handleRequest"
+  create_empty_function = true
+  kms_key_arn = aws_kms_key.this.arn
+
+  dead_letter_config = aws_sqs_queue.dlq.arn
+
+  environment_variables = {
+    var1 = "test"
+  }
+
+  timeout     = 30
+  memory_size = 128
+
+  policies = [
+    {
+      Effect = "Allow"
+
+      Action = [
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes",
+        "sqs:SendMessage"
+      ]
+
+      Resource = [
+        "arn:aws:sqs:eu-west-1:733041935482:*",
+      ]
+    },
+    {
+      Effect = "Allow"
+
+      Action = [
+        "xray:PutTraceSegments",
+        "xray:PutTelemetryRecords",
+        "xray:GetSamplingRules",
+        "xray:GetSamplingTargets",
+        "xray:GetSamplingStatisticSummaries"
+      ]
+
+      Resource = [
+        "arn:aws:xray:eu-west-1:733041935482:${aws_sqs_queue.lambda_example.name}",
+      ]
+    }
+
+  ]
+
+  source_mappings = [
+    {
+      enabled          = true
+      event_source_arn = "arn:aws:sqs:eu-west-1:733041935482:${aws_sqs_queue.lambda_example.name}"
+      batch_size       = 10
+    },
+  ]
+}
---
+++ lambda_function/lambda_function.tf
@@ -0,0 +1,53 @@
+data "aws_arn" "kms_key" {
+  arn = var.kms_key_arn
+}
+
+
+data "aws_arn" "dead_letter_config" {
+  arn = var.dead_letter_config
+}
+
+
+resource "aws_lambda_function" "lambda" {
+  function_name                  = "${var.function_name}"
+  description                    = "${var.description}"
+  role                           = "${aws_iam_role.lambda.arn}"
+  handler                        = "${var.handler}"
+  runtime                        = "${var.runtime}"
+  filename                       = "${var.create_empty_function ? "${path.module}/placeholder.zip" : var.filename}"
+  timeout                        = "${var.timeout}"
+  memory_size                    = "${var.memory_size}"
+  reserved_concurrent_executions = "${var.reserved_concurrent_executions}"
+  publish                        = "${var.publish}"
+
+
+  dynamic "dead_letter_config" {
+    for_each = data.aws_arn.dead_letter_config.arn == null ? [] : [true]
+    content {
+      target_arn = data.aws_arn.dead_letter_config.arn
+    }
+  }
+
+  tracing_config {
+    mode = "Active"
+  }
+
+  vpc_config {
+    subnet_ids         = ["${var.vpc_config["subnet_ids"]}"]
+    security_group_ids = ["${var.vpc_config["security_group_ids"]}"]
+  }
+
+  kms_key_arn = data.aws_arn.kms_key.arn
+  environment {
+    variables = "${var.environment_variables}"
+  }
+
+  tags = "${var.tags}"
+
+  lifecycle {
+    ignore_changes = [
+      filename,
+    ]
+  }
+}
+
